0a933f7016efa5b726ebcdd5de7b6d962af4d163
Matthew Nichols <mnichols@expedia.com>
2015-01-24 23:10:24 -0800

    updated CodeBook

--- CodeBook.md
+++ CodeBook.md
@@ -1,4 +1,9 @@
-# DATA DICTIONARY - Tidy UCI HAR Dataset
+# General Notes
+* From the original data set, we only retain features that are immediate means or standard deviations (those whose name end with "-mean()" or "-std()" in the original data set). We do not include fields with further calculations of means or stds
+* As in the original data set, measurement variables are normalized, and so are unitless. They are bounded by [-1,1]
+* The cleaning process also includes a step to take the mean of these measurements for each Subject and Activity. Accordingly, all data measurement variable names end in ".Mean.Mean" or ".Std.Mean" as they represent a mean of a mean, or a mean of a standard deviation, respectively
+
+# Variable Dictionary
 
 ## Subject
 The subject who performed the activity
--- generate_codebook.R
+++ generate_codebook.R
@@ -1,16 +1,32 @@
 generate_codebook <- function(names) {
-    # Header
-    string <- "# DATA DICTIONARY - Tidy UCI HAR Dataset"
+    # Overview
+    string <- "# General Notes\n"
+    
+    text <- c(
+    "* From the original data set, we only retain features that are immediate means or standard deviations ",
+    "(those whose name end with \"-mean()\" or \"-std()\" in the original data set). We do not include fields with ",
+    "further calculations of means or stds\n",
+    "* As in the original data set, measurement variables are normalized, and so are unitless. They are bounded by [-1,1]\n",
+    "* The cleaning process also includes a step to take the mean of these measurements for each Subject and Activity. ",
+    "Accordingly, all data measurement variable names end in \".Mean.Mean\" or \".Std.Mean\" as they represent a mean of ",
+    "a mean, or a mean of a standard deviation, respectively\n"
+    )
+    text <- paste(text,collapse="")
+    string <- paste(string,text,sep="")
+    
+    # Dictionary header
+    string <- paste(string,"# Variable Dictionary",sep="\n")
     # Subject and Activity field defitions
-    string <- paste(string,"**Subject**\n\tThe subject who performed the activity", sep="\n\n")
-    string <- paste(string,"**Activity**\n\tThe activity performed, of:", sep="\n\n")
-    string <- paste(string,"LAYING","SITTING","STANDING","WALKING","WALKING_DOWNSTAIRS","WALKING_UPSTAIRS", sep="\n\t\t")
+    string <- paste(string,"## Subject\nThe subject who performed the activity", sep="\n\n")
+    string <- paste(string,"## Activity\nThe activity performed, of:", sep="\n\n")
+    string <- paste(string,"* LAYING","* SITTING","* STANDING","* WALKING","* WALKING_DOWNSTAIRS",
+                    "* WALKING_UPSTAIRS", sep="\n")
     
     # Dynamically generate data field defintions
     for (name in names){
         # Field header
         string <- paste(string, "\n\n", sep="")
-        string <- paste(string,"**",name,"**\n\t",sep="")
+        string <- paste(string,"## ",name,"\n",sep="")
         string <- paste(string,"For the measurements taken of the subject performing the activity, the mean of the",sep="")
         
         # Mean or Std?
